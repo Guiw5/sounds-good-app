3e670d9d08460e1d3d666d236518ee5ddfb1ab61
Guido Wagner <wagner.guido.5@gmail.com>
2019-11-02 18:40:16 -0300

    fix url

--- src/http/client.js
+++ src/http/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'https://spotify.v1.api/',
+  baseURL: 'https://api.spotify.com/v1/',
   headers: { 'Content-Type': 'application/json' },
   responseType: 'json'
 })
